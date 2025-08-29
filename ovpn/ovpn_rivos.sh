798c5289ec2c2c682b64f3a8fed155f6d7ff9484
Dylan Reid <dgreid@dylanreid.com>
2025-08-29 16:49:43 -0700

    set dns in vpn container for work

--- ovpn/ovpn_rivos.sh
+++ ovpn/ovpn_rivos.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 podman run -v ~/rivosvpn/dylan.ovpn:/dylan.ovpn -v ~/rivosvpn/credentials.txt:/credentials.txt -it --rm --cap-add=NET_ADMIN --device /dev/net/tun --name rivosvpn ovpn_run
+sleep 10
+podman exec -it rivosvpn /bin/bash -c "echo nameserver 192.168.110.10 > /etc/resolv.conf"
